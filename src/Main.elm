4f9623f02651be99b47f83321adf2e31fee92fa1
wat-aro <kazutas1008@gmail.com>
2017-09-11 02:23:24 +0900

    Split modules

--- src/Main.elm
+++ src/Main.elm
@@ -1,37 +1,14 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
-
--- モデル
-
-type alias Model =
-    String
+import Messages exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
 
 init : ( Model, Cmd Msg )
 init =
     ( "Hello", Cmd.none )
-
--- メッセージ
-
-type Msg
-    = NoOp
-
--- ビュー
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
--- 更新
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-      NoOp ->
-          ( model, Cmd.none )
-
--- サブスクリプション(購読)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
